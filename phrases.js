e79322ebe3b41e1baa984c3edcde31286bb82f31
Rallade <rallade@hotmail.com>
2018-02-18 13:05:47 +0000

    Basic skeleton of conv engine WIP
    Unfinished

--- phrases.js
+++ phrases.js
@@ -2,11 +2,11 @@ var greetings = ['bonjour', 'salut', 'coucou'];
 
 var goodbyes = ['au revoir', 'salut' ,'à plus'];
 
-var system_intents = {'NAME': 0, 'AGE': 1, 'OCCUPATION': 2, 'ORIGINS': 3};
+var system_intents = ['GREET', 'NAME', 'AGE', 'OCCUPATION', 'ORIGINS', 'BYE'];
 
 var completed_intents = [];
 
-var user_intents = {'NAME': 0, 'AGE': 1, 'OCCUPATION': 2, 'ORIGINS': 4};
+var user_intents = ['GREET', 'NAME', 'AGE', 'OCCUPATION', 'ORIGINS', 'BYE'];
 
 var user_speaking = false;
 
@@ -18,43 +18,49 @@ var user_int = null;
 
 function parse(sent){
 	if (!start){
-                for (let greet of greetings){
-                        console.log(greet);
-                        if (sent.includes(greet)){
-                                start = true;
-                                if (user_speaking){
-                                        //reply
-                                }
-                        }
-                }
+		for (let greet of greetings){
+			console.log(greet);
+			if (sent.includes(greet)){
+				start = true;
+				if (user_speaking){
+					get_intent(sent);
 
-        } else {
-                for (let bye of goodbyes){
-                        if (sent.includes(bye)){
-                                end = true;
-                                if(user_speaking){
-                                        //reply
-                                }
-                        }
-                }
-	}
+				}
+			}
+		}
 
-}
-
-function gen_intent()(
-	if (!start){
-		start = true;
-		return greetings[0];
 	} else {
-		if (user_int == null){
-			sys_int = 'NAME';
-		} else {
-			
+		for (let bye of goodbyes){
+			if (sent.includes(bye)){
+				end = true;
+				if(user_speaking){
+					//reply
+				}
+			}
 		}
 	}
+
 }
 
+function gen_intent(){
+	if (!start){
+		start = true;
+		gen
+	} else {
+		if (user_int == null){
+			sys_int = system_intents[0];
+		} else {
+			for(let intention for system_intents){
+				if (completed_intents.indexOf(intention) < 0){
+					sys_int = system_intents[0];
+					break;
+				}
+			}
+		}
+	} else {
+		gen_response();
+	}
+}
+
+
 parse('hello');
-
-
-
