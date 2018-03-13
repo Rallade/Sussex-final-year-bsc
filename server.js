b9532e29003d3f1d206917c6124e6a3fb0fe4d52
Rallade <rallade@hotmail.com>
2018-03-13 16:04:14 +0000

    Front end prototype

--- server.js
+++ server.js
@@ -1,5 +1,28 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const app = require('./app.js')
 
-app.use(express.static(__dirname + '/public'))
+var exp = express();
 
-express.listen(3000);
+exp.use(bodyParser());
+
+exp.use(express.static(__dirname + '/public'));
+
+exp.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  next();
+})
+
+exp.post('/process', (req, res) => {
+  sent = req.body.sent;
+  console.log(14, sent);
+  app.process(app.createData(sent))
+    .then((analysis) => {
+      console.log(JSON.stringify(analysis));
+      res.send(JSON.stringify(analysis.sys_int, null, 4));
+    });
+})
+
+exp.listen(8080);
